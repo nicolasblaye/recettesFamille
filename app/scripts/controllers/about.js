aab4867fc6fd87e3b296a2309da1c9e68d4f9c0a
nblaye <nicolas.blaye@telecom-bretagne.eu>
2016-06-16 19:53:13 +0200

    init project

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name recettesFamilleApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the recettesFamilleApp
+ */
+angular.module('recettesFamilleApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name recettesFamilleApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the recettesFamilleApp
+ */
+angular.module('recettesFamilleApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
